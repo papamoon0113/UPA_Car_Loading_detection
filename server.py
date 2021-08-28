16a4781e4a18bcdc91ce3bedf66662b6960e1329
Moon-Ho-Jun <62269000+Moon-Ho-Jun@users.noreply.github.com>
2021-08-28 21:05:10 +0900

    'first_page_complete'

--- server.py
+++ server.py
@@ -146,4 +146,4 @@ def modal(location, model):
     return jsonify({'table_data':result})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080', debug=True)
+    app.run(host='0.0.0.0', port='8080', debug=False)
