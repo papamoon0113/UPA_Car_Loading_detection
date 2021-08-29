4eb4a612db096f5fa10f0d27e505e283f538adb0
Moon-Ho-Jun <62269000+Moon-Ho-Jun@users.noreply.github.com>
2021-08-30 00:38:13 +0900

    sibal

--- server.py
+++ server.py
@@ -213,4 +213,4 @@ def upload_xlsx():
     return redirect('/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='8080', debug=True)
+    app.run(host='0.0.0.0', port='8080', debug=False)
